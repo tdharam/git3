4cffa22634b77e546aed5ac01537b9f41905c418
tdharam.2910 <tdharam.2910@gmail.com>
2015-07-29 16:06:02 +0530

    test

---
+++ StringPool/MemorySpec.java
@@ -0,0 +1,42 @@
+package StringPool;
+
+import Practice1.Employee;
+// java 7
+public class MemorySpec {
+
+	//Run time constant Pool
+	static final int CAPACITY = 16;
+	
+	//method Area
+	static int size = 10;
+	
+	//string pool which is part of Run time constant Pool 
+	String name = "dharam";
+	 
+	//heap
+	String comapny = new String("cybage");
+	
+	//heap
+	int value;
+	
+	
+
+	void execute() {
+		int i ;//Stack (in Thread's frame for this method )
+		 
+		//string pool which is part of Run time constant Pool(every frame has reffrence to run time contant pool)
+		String myName = "dharam";
+		System.out.println(name==myName);//ture
+		
+		//heap
+		String myAddress = new String("d-909");
+
+	}
+
+}
+//http://javarevisited.blogspot.in/2013/01/difference-between-stack-and-heap-java.html
+
+/*/Each frame has its own 
+array of local variables ,
+operand stack  and 
+a reference to the runtime constant pool (§2.5.5) of the class of the current method.*/
---
+++ StringPool/MyMain.java
@@ -0,0 +1,38 @@
+package StringPool;
+
+public class MyMain {
+
+	public static void main(String[] args) {
+ 
+		
+		
+		MemorySpec m = new MemorySpec( );
+		m.execute();
+		
+	 
+		
+		/*String s1= "abc";
+		String s2= new String("abc");
+		String s3= "abc";
+		String s4= new String ("abc");
+		
+		if (s4==s2)
+			System.out.println("true");
+		else
+			System.out.println("false");*/
+		/*try {
+			for(int i=0;i<1000000000;i++){
+				String s= new String("SimplwExamplenumber"+i);
+				
+				s.intern();
+				System.out.println(s);
+			}
+		} catch (Exception e) {
+			 e.printStackTrace();
+		}*/
+		
+		
+
+	}
+
+}
